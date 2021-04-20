43aa65cf07b0027c0411f237f7fb1ce3f29d6518
Gino Zaccardelli <ginozacc@gmail.com>
2021-04-20 11:56:34 -0400

    Add project files.

---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using static System.Console;
+
+namespace NumberGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int winNum = r.Next(1, 101);
+
+            bool win = false;
+
+            //WriteLine(winNum);
+
+            int i = 0;
+
+            do
+            {
+                Write("Guess a number between 1 and 100: ");
+                string s = ReadLine();
+
+                try
+                {
+                    i = int.Parse(s);
+                }
+                catch (Exception e)
+                {
+                    switch (e)
+                    {
+                        case FormatException:
+                        case OverflowException:
+                        case ArgumentNullException:
+                            WriteLine("Please enter a number between 1 and 100!");
+                            break;
+                        default:
+                            WriteLine(e);
+                            break;
+                    }
+                }
+
+                if (i > winNum)
+                {
+                    WriteLine("Too high! Guess lower...");
+
+                }
+                else if (i < winNum)
+                {
+                    WriteLine("Too low! Guess higher...");
+                }
+                else if (i == winNum)
+                {
+                    WriteLine("You WIN!");
+                    win = true;
+                }
+            } while (win == false);
+
+            WriteLine("Press any key to exit...");
+            ReadKey();
+        }
+    }
+}
